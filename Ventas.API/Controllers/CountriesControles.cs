1663c002de388923095d132900aff3ba5c10aebb
sonrie0 <20030263@itesa.edu.mx>
2025-03-07 15:49:19 -0600

    Version 03: Constroladores de API para evitar  su consumo , post y get

---
+++ Ventas.API/Controllers/CountriesControles.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ventas.API.Data;
+using Ventas.Shared.Entities;
+
+namespace Ventas.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/conutries")]
+    public class CountriesControlles : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesControlles(DataContext context)
+        {
+            this._context = context;
+        }
+    
+
+         [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(PostAsync(country));
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Getasync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+}
+
+
+
+}
