af9f9957cf9028ef57c520fd2fe5ba45f6078269
sonrie0 <20030263@itesa.edu.mx>
2025-03-07 15:11:18 -0600

    Version _02: Se creo la BD con EFC

---
+++ Ventas.API/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ventas.Shared.Entities;
+
+namespace Ventas.API.Data
+{
+    public class DataContext:DbContext
+    {
+        // debe heredar de db context
+        // conexion con la bd 
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c=>c.Name).IsUnique(); 
+            //El nombre es unico, esccribes dos veces el mismo no dejara 
+            //Esto no deja escribir lo msmo 2 vcs
+        }
+
+    }
+}
---
+++ Ventas.Shared/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name="País")]
+        [MaxLength(100,ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] 
+        public string Name { get; set; } = null!;
+        
+        
+    }
+}
